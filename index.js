a318fa84b160e4ca61c4689bb984f1404e22bba0
kent winter <kgw700@gmail.com>
2017-11-19 00:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var receipes = {
-  eggs: '3',
-  flour: '3 cups'
-  };
-  console.log (typeof(recipes))
+var receipes = {};
 
 function updateObjectWithKeyAndValue (object, key, value) {
  return Object.assign({}, object, {[key]: value})
