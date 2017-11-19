6ae5532fd5fc7e3b087422b7c675902c95135ac0
kent winter <kgw700@gmail.com>
2017-11-19 01:25:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function deleteFromObjectByKey (object, key) {
   delete newobject[key];
   return newobject;
 }
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object[key];
+  return object;
+}
