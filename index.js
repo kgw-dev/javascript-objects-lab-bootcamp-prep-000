84e2211225bdaeab3ab018b7dd8bc5e7e8cb2ad9
kent winter <kgw700@gmail.com>
2017-11-19 00:55:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var receipes = {
+var recipes = {
   eggs: '3',
   flour: '1 cup',
   sugar: '3 tablespoons'
@@ -6,4 +6,14 @@ var receipes = {
 
 function updateObjectWithKeyAndValue (object, key, value) {
  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyupdateObjectWithKeyAndValue (object, key, value) {
+objects[key]=value;
+ return object;
+}
+
+function deleteFromObjectByKey (object, key) {
+delete object.key;
+ return object;
 }
