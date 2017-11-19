b4cfb0726d73eebc7597e0a452f90a621ddd4958
kent winter <kgw700@gmail.com>
2017-11-19 00:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var receipes = {};
+var receipes = {
+  eggs: '3',
+  flour: '1 cup',
+  sugar: '3 tablespoons'
+}
 
 function updateObjectWithKeyAndValue (object, key, value) {
  return Object.assign({}, object, {[key]: value})
