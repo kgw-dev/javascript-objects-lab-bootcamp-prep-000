de25628af52b2ba74ea82a0ef19330f040470b5c
kent winter <kgw700@gmail.com>
2017-11-19 00:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue (object, key, value) {
- return Object.assign({}, object, {[key]: value})
+ return object.assign({}, object, {[key]: value})
 }
 
-function destructivelyupdateObjectWithKeyAndValue (object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 objects[key]=value;
  return object;
 }
